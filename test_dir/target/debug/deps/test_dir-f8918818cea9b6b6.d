13b8b3ea7f9ffca6cc97509ef30604d307ef0bfa
David Petre <davidpetre@uchicago.edu>
2023-10-22 16:28:09 -0500

    added_dir

---
+++ test_dir/target/debug/deps/test_dir-f8918818cea9b6b6.d
@@ -0,0 +1,5 @@
+/Users/davidmac/LocalProjects/rust/kalshi-rust/test_dir/target/debug/deps/test_dir-f8918818cea9b6b6.rmeta: src/main.rs
+
+/Users/davidmac/LocalProjects/rust/kalshi-rust/test_dir/target/debug/deps/test_dir-f8918818cea9b6b6.d: src/main.rs
+
+src/main.rs:
