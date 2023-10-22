6f875c9a8e85a97675998e2eef273fdc61bdbd1d
David Petre <davidpetre@uchicago.edu>
2023-10-22 16:31:03 -0500

    removed target dir

--- test_dir/target/debug/deps/test_dir-548c276b2dfb84e8.d
+++
@@ -1,5 +0,0 @@
-/Users/davidmac/LocalProjects/rust/kalshi-rust/test_dir/target/debug/deps/test_dir-548c276b2dfb84e8.rmeta: src/main.rs
-
-/Users/davidmac/LocalProjects/rust/kalshi-rust/test_dir/target/debug/deps/test_dir-548c276b2dfb84e8.d: src/main.rs
-
-src/main.rs:
--- test_dir/target/debug/deps/test_dir-f8918818cea9b6b6.d
+++
@@ -1,5 +0,0 @@
-/Users/davidmac/LocalProjects/rust/kalshi-rust/test_dir/target/debug/deps/test_dir-f8918818cea9b6b6.rmeta: src/main.rs
-
-/Users/davidmac/LocalProjects/rust/kalshi-rust/test_dir/target/debug/deps/test_dir-f8918818cea9b6b6.d: src/main.rs
-
-src/main.rs:
