4cc732f256722e550afdc880be5d5209615ddd54
David Petre <davidpetre@uchicago.edu>
2023-10-22 19:10:25 -0500

    "Completed Login Function, laid down some work!"

--- kalshi/src/lib.rs
+++ kalshi/src/lib.rs
@@ -1,14 +1,87 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+const LOGOUT_URL: &str = "https://trading-api.kalshi.com/trade-api/v2/logout";
+// imports
+use reqwest;
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+
+// Main Implementation, plan to abstract out in the future
+#[derive(Debug)]
+pub struct Kalshi {
+    logged_in: bool,
+    curr_token: Option<String>,
+    member_id: Option<String>,
+    client: reqwest::Client
 }
 
+#[derive(Debug, Serialize, Deserialize)]
+struct LoginResponse {
+    member_id: String, 
+    token: String
+}
+
+impl Kalshi {
+
+    pub fn new() -> Kalshi {
+        return Kalshi{
+            logged_in: false,
+            curr_token: None,
+            member_id: None,
+            client: reqwest::Client::new()
+        };
+    }
+
+    pub async fn login(&mut self, user: &str, password: &str) -> Result<(), reqwest::Error> {
+        const LOGIN_URL: &str = "https://trading-api.kalshi.com/trade-api/v2/login";
+        let mut login_payload = HashMap::new();
+        login_payload.insert("email", user);
+        login_payload.insert("password", password);
+
+
+        let result: LoginResponse =  self.client
+                                        .post(LOGIN_URL)
+                                        .json(&login_payload)
+                                        .send()
+                                        .await?
+                                        .json()
+                                        .await?;
+        
+        self.curr_token = Some(result.token);
+        self.member_id = Some(result.member_id);
+        self.logged_in = true;
+
+        return Ok(())
+    }
+
+    pub async fn logout(&self) -> Result<(), Box<dyn std::error::Error>> {
+        const LOGOUT_URL: &str = "https://trading-api.kalshi.com/trade-api/v2/logout";
+        return Ok(()) 
+    }
+
+    pub fn get_user_token(&self) -> Option<String> {
+        match &self.curr_token {
+            Some(val) => return Some(val.clone()),
+            _ => return None
+        }
+    }
+
+}
+
+
+// Enums
+pub enum ConnectionError {
+    ClientConnectionFailure,
+    IncorrectCredentials,
+    ServerConnectionFailure
+}
+
+// Tests
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = 4;
         assert_eq!(result, 4);
     }
 }
--- test_dir/src/main.rs
+++ test_dir/src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use kalshi::Kalshi;
+use std::env;
+use dotenv::dotenv;
+use futures::executor::block_on;
+        
+extern crate kalshi;
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    
+
+    let mut username = "dummy".to_string();
+    let mut password = "dummy".to_string();
+    
+    if let Ok(key) = env::var("PASSWORD") {
+        println!("got password");
+        password = key;
+    }
+    if let Ok(user) = env::var("USER_NAME") {
+        println!("got user");
+        username = user;
+    }
+    // main testing logic, ignoring unit tests for now
+    let mut kalshi_instance = Kalshi::new();
+    kalshi_instance.login(&username, &password).await;
 }
