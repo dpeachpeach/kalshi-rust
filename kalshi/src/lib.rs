02e0330e6d5bff33c4c01856c99331342ccf2c68
David Petre <davidpetre@uchicago.edu>
2023-10-25 18:34:51 -0500

    support for demo vs. live added + housekeeping

--- kalshi/src/lib.rs
+++ kalshi/src/lib.rs
@@ -4,34 +4,19 @@ use serde::{Serialize, Deserialize};
 
 // Main Implementation, plan to abstract out in the future
 #[derive(Debug)]
-pub struct Kalshi {
+pub struct Kalshi<'a> {
+    base_url: &'a str, 
     logged_in: bool,
     curr_token: Option<String>,
     member_id: Option<String>,
     client: reqwest::Client
 }
-// used in login method
-#[derive(Debug, Serialize, Deserialize)]
-struct LoginResponse {
-    member_id: String, 
-    token: String
-}
-// used in login method
-#[derive(Debug, Serialize, Deserialize)]
-struct LoginPayload {
-    email: String, 
-    password: String
-}
 
-#[derive(Debug, Serialize, Deserialize)]
-struct BalanceResponse {
-    balance: i64 
-}
+impl<'a> Kalshi<'a> {
 
-impl Kalshi {
-
-    pub fn new() -> Kalshi {
-        return Kalshi{
+    pub fn new() -> Kalshi<'a> {
+        return Kalshi {
+            base_url: "",
             logged_in: false,
             curr_token: None,
             member_id: None,
@@ -39,9 +24,20 @@ impl Kalshi {
         };
     }
 
+    pub fn build_base_url(&mut self, trading_env: TradingEnvironment) -> (){
+        match trading_env {
+            TradingEnvironment::LiveMarketMode => {
+                self.base_url = "https://trading-api.kalshi.com/trade-api/v2";
+            },
+            TradingEnvironment::DemoMode => {
+                self.base_url = "https://demo-api.kalshi.co/trade-api/v2";
+            }
+        }
+    }
+
     pub async fn login(&mut self, user: &str, password: &str) -> Result<(), reqwest::Error> {
 
-        const LOGIN_URL: &str = "https://trading-api.kalshi.com/trade-api/v2/login";
+        let login_url: &str = &format!("{}/login", self.base_url.to_string());
 
         let login_payload = LoginPayload {
             email: user.to_string(),
@@ -49,7 +45,7 @@ impl Kalshi {
         };
     
         let result: LoginResponse =  self.client
-                                        .post(LOGIN_URL)
+                                        .post(login_url)
                                         .json(&login_payload)
                                         .send()
                                         .await?
@@ -64,10 +60,13 @@ impl Kalshi {
     }
 
     pub async fn logout(&self) -> Result<(), reqwest::Error> {
-        const LOGOUT_URL: &str = "https://trading-api.kalshi.com/trade-api/v2/logout";
+
+        //const LOGOUT_URL: &str = "https://trading-api.kalshi.com/trade-api/v2/logout";
+        
+        let logout_url: &str = &format!("{}/logout", self.base_url.to_string());
 
         self.client
-                .post(LOGOUT_URL)
+                .post(logout_url)
                 .header("Authorization", self.curr_token.clone().unwrap())
                 .header("content-type", "application/json".to_string())
                 .send()
@@ -78,10 +77,12 @@ impl Kalshi {
 
     pub async fn get_balance(&self) -> Result<i64, reqwest::Error> {
 
-        let BALANCE_URL = "https://trading-api.kalshi.com/trade-api/v2/portfolio/balance";
+        //const BALANCE_URL: &str = "https://trading-api.kalshi.com/trade-api/v2/portfolio/balance";
+
+        let balance_url: &str = &format!("{}/portfolio/balance", self.base_url.to_string()); 
 
         let result:BalanceResponse = self.client
-                .get(BALANCE_URL)
+                .get(balance_url)
                 .header("Authorization", self.curr_token.clone().unwrap())
                 .send()
                 .await?
@@ -93,6 +94,16 @@ impl Kalshi {
         return Ok(bal)
     }
 
+    pub async fn get_exchange_status(&self) -> Result<ExchangeStatus, reqwest::Error> {
+        todo!()
+    }
+
+    /* 
+    pub async fn get_exchange_schedule(&self) -> Result<ExchangeSchedule, reqwest::Error> {
+        todo!()
+    }
+    */
+
     pub fn get_user_token(&self) -> Option<String> {
         match &self.curr_token {
             Some(val) => return Some(val.clone()),
@@ -100,18 +111,57 @@ impl Kalshi {
         }
     }
 
+    fn build_url(&self, endpoint: &str) -> String {
+        todo!()
+    }
+
+
 }
 
 
+// structs
+// used in login method
+#[derive(Debug, Serialize, Deserialize)]
+struct LoginResponse {
+    member_id: String, 
+    token: String
+}
+// used in login method
+#[derive(Debug, Serialize, Deserialize)]
+struct LoginPayload {
+    email: String, 
+    password: String
+}
+// used in getbalance method
+#[derive(Debug, Serialize, Deserialize)]
+struct BalanceResponse {
+    balance: i64 
+}
+
+// used in get_exchange_status
+#[derive(Debug, Serialize, Deserialize)]
+struct ExchangeStatus {
+    trading_active: bool,
+    exchange_active: bool,
+}
+
+// used in get_exchange_schedule
+#[derive(Debug, Serialize, Deserialize)]
 
 // Enums
+pub enum TradingEnvironment {
+    DemoMode,
+    LiveMarketMode
+}
+
+// legacy code for now
 pub enum ConnectionError {
     ClientConnectionFailure,
     IncorrectCredentials,
     ServerConnectionFailure
 }
 
-// Tests
+// unit tests, absent at the moment. all test logic is handled in the test bot dir 
 #[cfg(test)]
 mod tests {
 
--- test_dir/src/main.rs
+++ test_dir/src/main.rs
@@ -1,31 +1,43 @@
-use futures::TryFutureExt;
+use dotenv::dotenv;
 use kalshi::Kalshi;
 use std::env;
-use dotenv::dotenv;
-        
+
 extern crate kalshi;
 
+enum APIType {
+    Live,
+    Demo
+}
+fn retreive_credentials(setting: APIType, username: &mut String, pass: &mut String) -> () {
+    match setting {
+        APIType::Live => {
+        },
+        APIType::Demo => {
+            if let Ok(key) = env::var("DEMO_PASSWORD") {
+                println!("got password");
+                *pass = key;
+            }
+            if let Ok(user) = env::var("DEMO_USER_NAME") {
+                println!("got user");
+                *username = user;
+            }
+        }
+    }
+}
 #[tokio::main]
 async fn main() {
     dotenv().ok();
-    
 
     let mut username = "dummy".to_string();
     let mut password = "dummy".to_string();
-    
-    if let Ok(key) = env::var("PASSWORD") {
-        println!("got password");
-        password = key;
-    }
-    if let Ok(user) = env::var("USER_NAME") {
-        println!("got user");
-        username = user;
-    }
+    retreive_credentials(APIType::Demo, &mut username, &mut password);
+
     // main testing logic, ignoring unit tests for now
     let mut kalshi_instance = Kalshi::new();
+    kalshi_instance.build_base_url(kalshi::TradingEnvironment::DemoMode);
     kalshi_instance.login(&username, &password).await;
     let token = kalshi_instance.get_user_token().unwrap();
-    println!("{}",token);
+    println!("{}", token);
     let balance = kalshi_instance.get_balance().await.unwrap();
     println!("{}", balance);
 }
